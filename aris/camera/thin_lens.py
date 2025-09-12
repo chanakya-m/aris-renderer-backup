3686f6133a37d34771251d6f559df14ab2d7a520
chanakya-m <mchanakya@gmail.com>
2025-09-11 20:26:17 -0400

    code complete

--- aris/camera/thin_lens.py
+++ aris/camera/thin_lens.py
@@ -1,4 +1,6 @@
+import torch
 from torch import Tensor
+import torch.nn.functional as F
 
 from aris.camera import Camera, camera_registry
 from aris.camera.perspective import PerspectiveCamera
@@ -18,31 +20,25 @@ class ThinLensCamera(Camera):
         self.focal_distance = focal_distance
 
     def image_to_rays(self, coords: Tensor) -> tuple[Tensor, Tensor]:
-        if self.aperture == 0:
-            return self.p_cam.image_to_rays(coords)
+        if self.aperture == 0.0:
+            return self.camera.image_to_rays(coords)
 
-        rays_d_pinhole = self.p_cam.image_to_camera(coords)
+        pinhole_rays_d = self.camera.image_to_camera(coords)
+        pinhole_rays_o = torch.zeros_like(pinhole_rays_d)
 
-        z_comp = rays_d_pinhole[:, 2:3]
-        ft = -self.focal_distance / (z_comp + 1e-8)
-        p_focus = rays_d_pinhole * ft
+        ft = -self.focal_distance / (pinhole_rays_d[:, 2:3] + 1e-8)
+        p_focus = pinhole_rays_o + ft * pinhole_rays_d
 
-        n_rays = len(coords)
         lens_radius = self.aperture / 2.0
+        N = coords.shape[0]
 
-        p_lens_unit = 2 * torch.rand(n_rays, 2, device=coords.device) - 1
-        p_lens_scaled = p_lens_unit * lens_radius
+        lens_points_2d = (torch.rand(N, 2, device=coords.device) * 2.0 - 1.0) * lens_radius
 
-        rays_o_camera = torch.cat([
-            p_lens_scaled,
-            torch.zeros_like(p_lens_scaled[:, 0:1])
-        ], dim=1)
+        new_rays_o = torch.cat([lens_points_2d, torch.zeros(N, 1, device=coords.device)], dim=1)
 
-        rays_d_camera = p_focus - rays_o_camera
+        new_rays_d = p_focus - new_rays_o
+        new_rays_d = F.normalize(new_rays_d, p=2, dim=1)
 
-        norm = torch.linalg.norm(rays_d_camera, dim=1, keepdim=True)
-        rays_d_camera = rays_d_camera / (norm + 1e-8)
-
-        return self.p_cam.camera_to_world(rays_o_camera, rays_d_camera)
+        return self.camera.camera_to_world(new_rays_o, new_rays_d)
 
 camera_registry.add("thin", ThinLensCamera)
